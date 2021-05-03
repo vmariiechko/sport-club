faf84de354097b10cbb0252d64903e6aee5d328d
Vadym Mariiechko <vadimich348@gmail.com>
2021-05-03 16:26:22 +0200

    Prepare project for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sport_club.wsgi --log-file -
--- sport_club/settings.py
+++ sport_club/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sport-club-dev.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
