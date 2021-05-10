fb81fbaba6650d6265963c005ee45baffd202c7a
Oleksii <svjjdnv@gmail.com>
2021-05-10 21:23:39 +0200

    home page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Link, useHistory} from 'react-router-dom';
+import { ReactSVG } from 'react-svg'
 import classes from './Header.module.css';
+import logo from '../../assets/images/logo.svg';
 
 const Header = (props) => {
     return (
         <div className={classes.Header}>
-            <Link to='/'>Home</Link>
-            <Link to='/products'>Products</Link>
-            <Link to='/trainers'>Trainers</Link>
-            <Link to='/profile'>Your profile</Link>
+            <div className={classes.HeaderInner}>
+                <div className={classes.Logo}>
+                    <ReactSVG src={logo} />
+                </div>
+                <div className={classes.Navbar}>
+                    <Link to='/'>News</Link>    
+                    <Link to='/'>Programs</Link>
+                    <Link to='/'>Trainers</Link>
+                    <Link to='/'>About us</Link>
+                    <Link to='/'>Contact</Link>
+                    <Link to='/profile'>Account</Link>
+                </div>
+            </div>
         </div>
     );
 };
