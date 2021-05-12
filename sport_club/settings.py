cc0d79f9aaeabe93073100544c89bc38bb6921d4
Vadym Mariiechko <vadimich348@gmail.com>
2021-05-12 18:29:21 +0200

    Configure media folder

--- sport_club/settings.py
+++ sport_club/settings.py
@@ -141,6 +141,12 @@ STATICFILES_DIRS = [
     BASE_DIR / 'client/build/static'
 ]
 
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- sport_club/urls.py
+++ sport_club/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,6 @@ urlpatterns = [
     path('api/', include('cards_api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Allow to work with media folder localy
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
