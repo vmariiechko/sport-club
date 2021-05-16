09a295fbfafb946008153daa59ac00433d6c63df
Oleksii <svjjdnv@gmail.com>
2021-05-16 22:01:24 +0200

    home page scroll, api /posts + /passes

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,14 @@ import {BrowserRouter as Router } from 'react-router-dom';
 import store from './store/store';
 import './index.css';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
